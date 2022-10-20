e3a6cc94d5ef804670606b74867aad31664d5cb2
Ak <akachukwumba@gmail.com>
2022-10-20 22:31:35 +0200

    Changed the database from sqlite to mysql

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 import yaml
 import json
 from flask import Flask, render_template
-import sqlite3
+import mysql.connector
+import variables
+
 
 
 
@@ -14,35 +16,31 @@ app = Flask(__name__)
 def get_response():
     request = requests.get(api_link)
     json_response = request.json()
-    return(json_response)
-
-def json_to_yaml():
-    yaml_file = open('response.yaml', 'w')
-    yaml.dump(get_response(), yaml_file, default_flow_style=False)
-    yaml_response = open('response.yaml', mode='r')
-    yaml_reponse = (yaml_response.read())
-    return(yaml_reponse)
-
+    yaml_response = yaml.dump(json_response, default_flow_style=False)
+    return(yaml_response)
 
 
 
 @app.route('/')
 def index():
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    content = (f"{json_to_yaml()}")
-    def create_table():
-        cur.execute('CREATE TABLE IF NOT EXISTS tech_phrases (id INTEGER PRIMARY KEY AUTOINCREMENT, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, content TEXT NOT NULL);')
+    mydb = mysql.connector.connect(
+        host=variables.host,
+        user=variables.user,
+        password=variables.password,
+        database=variables.database
+        )   
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE TABLE IF NOT EXISTS tech_phrases (ID INT AUTO_INCREMENT PRIMARY KEY, CREATED TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, CONTENT TEXT NOT NULL)")
+    content = (get_response())
+    
 
     def data_entry():
-        
-        cur.execute("INSERT INTO tech_phrases (content) VALUES(?)",
-                                                        (content,))
-        conn.commit()
-    create_table()
+        sql = "INSERT INTO tech_phrases (content) VALUES(%s)"
+        val = [(content)]
+        mycursor.execute(sql, val)
+        mydb.commit()
     data_entry()
-    cur.close()
-    conn.close()
+    mydb.close()
     return render_template(f'index.html', variable=(content))
     
 if __name__ == "__main__":
